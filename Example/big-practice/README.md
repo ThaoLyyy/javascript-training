354f90b446e63af8e6705112e6817d870723f7d9
ThaoLyyy <thaolydn1805@gmail.com>
2022-05-30 18:53:52 +0700

    new code practice

---
+++ Example/big-practice/README.md
@@ -0,0 +1,75 @@
+# PRACTICE JAVASCRIPT
+
+## Overview
+
+- Create an application with Javascript
+
+## Techinical
+
+- HTML5
+- CSS3
+- Javascript
+- DOM
+- MVC
+
+## Editor
+
+- Visual Studio Code
+
+## Target
+
+- Understand and apply ES6 syntax
+- Practice DOM manipulation
+- Understand and apply basic asynchronous and synchronous
+- Use JSON server for working with full fake REST API
+
+## Timeline
+
+- Estimate time: 6 days
+
+## Desing
+
+- [Figma](https://www.figma.com/file/vvPw8UG7QVPG46FgsCMc1s/Course-Management)
+
+## Geting started
+
+- Clone repository
+
+```
+git clone https://github.com/ThaoLyyy/javascript-training
+```
+
+- Checkout branch `develop`
+
+```
+git checkout develop
+```
+
+- Change directory to `big-pratice` folder
+
+```
+cd big_practice
+```
+
+- Open cmd and install package
+
+```
+npm install
+```
+
+- Run application
+
+```
+npm start
+```
+
+- Open browser with the link below
+
+```
+http://localhost:1234
+```
+
+## Author
+
+- Thao Ly
+- Email: thaolydn1805@gmail.com
---
+++ Example/big-practice/src/js/main.js
@@ -0,0 +1,73 @@
+// const {image, title, author, rating...addPost.} = course;
+const endpoint = "http://localhost:3456/courses";
+const courseList = document.querySelector(".course-list");
+const formPost = document.querySelector(".form__post");
+const formSubmit = document.querySelector(".form-submit");
+const filterInput = document.querySelector(".main-header__filter");
+let updateId = null;
+// 
+function debounceFn(func, wait, immediate) {
+  let timeout;
+  return function () {
+    let context = this,
+      args = arguments;
+    let later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+filterInput.addEventListener(
+  "keydown",
+  debounceFn(function (e) {
+    let path = endpoint;
+    if (e.target.value !== "") {
+      path = `${endpoint}?title_like=${e.target.value}`;
+    }
+    getCourses(path);
+  }, 500)
+);
+
+async function addNewCourse({
+  image,
+  title,
+  author,
+  rating,
+  price,
+  bestSeller,
+  buyAmount,
+}) {
+  await fetch(endpoint, {
+    method: "POST",
+    body: JSON.stringify({
+      image,
+      title,
+      author,
+      rating,
+      price,
+      bestSeller,
+      buyAmount,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+}
+
+getCourses();
+// json-server --watch db.json
+/**
+ * Course
+ * image
+ * title
+ * author
+ * rating
+ * price
+ * bestSeller
+ * buyAmount
+ */
