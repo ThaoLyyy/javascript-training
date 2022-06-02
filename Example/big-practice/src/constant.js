ecadd86cf1212174318d3745075fa7b6c38fbbca
ThaoLyyy <thaolydn1805@gmail.com>
2022-06-02 17:56:01 +0700

    feat: Update addcourse

---
+++ Example/big-practice/src/constant.js
@@ -0,0 +1,7 @@
+const API_HOST = '"http://localhost:3456/courses'
+const PATH = 'courses'
+
+export default {
+    API_HOST,
+    PATH
+}
---
+++ Example/big-practice/src/js/views/course.js
@@ -0,0 +1,67 @@
+import fetch from "../helpers/service";
+import path from "../constant";
+
+export default class CourseModel {
+  constructor() {
+    this.courses = [];
+  }
+
+  async getCourse() {
+    const course = await fetch.get(`/${path.PATH_COURSE}`);
+    return course;
+  }
+
+  async addNewCourse(
+      image,
+      title,
+      author,
+      rating,
+      price,
+      bestSeller,
+      buyAmount,
+    ) {
+      await fetch(PATH_COURSE, {
+        method: "POST",
+        body: JSON.stringify({
+          image,
+          title,
+          author,
+          rating,
+          price,
+          bestSeller,
+          buyAmount,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+    }
+
+    //Edit course
+    async updateCourse(
+        id,
+        image,
+        title,
+        author,
+        rating,
+        price,
+        bestSeller,
+        buyAmount,
+      ) {
+        await fetch(`${PATH_COURSE}/${id}`, {
+          method: "PUT",
+          body: JSON.stringify({
+            image,
+            title,
+            author,
+            rating,
+            price,
+            bestSeller,
+            buyAmount,
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        });
+      }
+}
