7598cf5bb4fb72230b56925a93d08020feb47698
ThaoLy185 <89328896+ThaoLyyy@users.noreply.github.com>
2022-06-13 18:09:55 +0700

    Implement search function

--- big-practice/src/js/controllers/course.js
+++ big-practice/src/js/controllers/course.js
@@ -5,30 +5,54 @@ export default class Controller {
 
     this.model.bindCourseListChanged(this.onCourseListChanged);
     this.view.bindAddNewCourse(this.handleAddNewCourse);
+    // this.view.bindDeleteCourse(this.handleDeleteCourse);
+    // this.onCourseListChanged(this.model.showCourse);
+    this.view.bindFilterCourse(this.handleFilterCourse);
+
     this.init();
+    // init();
   }
 
   init = async () => {
     const courses = await this.model.showCourse();
     this.view.display(courses);
+    return courses;
   };
 
   onCourseListChanged = (courses) => {
     this.view.display(courses);
   };
-  handleAddNewCourse = ( image,
+  handleAddNewCourse = (
+    image,
+    title,
+    author,
+    rating,
+    price,
+    buyAmount,
+    bestSeller
+  ) => {
+    this.model.addNewCourse(
+      image,
       title,
       author,
       rating,
       price,
       buyAmount,
-      bestSeller,) => {
-    this.model.addNewCourse( image,
-      title,
-      author,
-      rating,
-      price,
-      buyAmount,
-      bestSeller,);
+      bestSeller
+    );
   };
+
+  // handleDeleteCourse = async (id) => {
+  //   const courses = await this.model.deleteCourse(id);
+  //   this.view.display(courses);
+  // };
+  handleSearchCourse = async title => {
+    const courses = await this.model.searchCourse(title)
+    this.view.display(courses)
+  }
+
+  handleFilterCourse = async title =>{
+    const courses = await this.model.filterCourse(title)
+    this.view.display(courses)
+  }
 }
