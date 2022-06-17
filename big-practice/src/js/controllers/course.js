370041f084bbcae21b6691dc0369464ac323776c
ThaoLy185 <89328896+ThaoLyyy@users.noreply.github.com>
2022-06-18 00:50:56 +0700

    Update function and Delete function

--- big-practice/src/js/controllers/course.js
+++ big-practice/src/js/controllers/course.js
@@ -5,20 +5,21 @@ export default class Controller {
 
     this.view.bindAddNewCourse(this.handleAddNewCourse);
     this.view.bindFilterCourse(this.handleFilterCourse);
-
+    this.view.bindUpdateCourse(this.handleUpdateCourse);
+    this.view.bindDeleteCourse(this.handleDeleteCourse);
 
     this.init();
   }
-
   init = async () => {
-   await this.model.getCourses();
-   this.onCourseListChanged(this.model.courses);
+    await this.model.getCourses();
+    this.onCourseListChanged(this.model.courses);
   };
 
   onCourseListChanged = (courses) => {
     this.view.displayCourses(courses);
   };
-  handleAddNewCourse = async(
+
+  handleAddNewCourse = async (
     image,
     title,
     author,
@@ -27,7 +28,7 @@ export default class Controller {
     buyAmount,
     bestSeller
   ) => {
-   const course = await this.model.addNewCourse(
+    const course = await this.model.addNewCourse(
       image,
       title,
       author,
@@ -37,16 +38,45 @@ export default class Controller {
       bestSeller
     );
     this.onCourseListChanged(this.model.courses);
-
   };
 
-  handleSearchCourse = async title => {
-    const courses = await this.model.searchCourse(title)
-    this.view.displayCourses(courses)
-  }
+  handleUpdateCourse = async (
+    image,
+    title,
+    author,
+    rating,
+    price,
+    buyAmount,
+    bestSeller
+  ) => {
+    const course = await this.model.updateCourse(
+      image,
+      title,
+      author,
+      rating,
+      price,
+      buyAmount,
+      bestSeller
+    );
+    this.onCourseListChanged(this.model.courses);
+  };
 
-  handleFilterCourse = async title =>{
-    const courses = await this.model.filterCourse(title)
-    this.view.displayCourses(courses)
-  }
+   /**
+    * Use id from views call to models 
+    * @param {string} id 
+    */
+    handleDeleteCourse = async id => {
+    const courses = await this.model.deleteCourse(id);
+    this.view.displayCourses(courses);
+}
+  
+  handleSearchCourse = async (title) => {
+    const courses = await this.model.searchCourse(title);
+    this.view.displayCourses(courses);
+  };
+
+  handleFilterCourse = async (title) => {
+    const courses = await this.model.filterCourse(title);
+    this.view.displayCourses(courses);
+  };
 }
