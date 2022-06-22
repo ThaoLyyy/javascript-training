be74dcb6b84a1344dd7ff5d7f4b99e417e989237
ThaoLy185 <89328896+ThaoLyyy@users.noreply.github.com>
2022-06-22 18:12:50 +0700

    Fix  create getCourseById

--- big-practice/src/js/models/course.js
+++ big-practice/src/js/models/course.js
@@ -1,29 +1,24 @@
 import fetch from "../helpers/service";
 import path from "../constant";
+import { v4 as uuidv4 } from "uuid";
 
 export default class Model {
-  courses = [];
-  constructor() {}
+  constructor() {
+    this.courses = [];
+  }
 
   /**
    * Use API url from fetch import in read data
    * @returns {array} course.
    */
-   getCourses = async () => {
+  getCourses = async () => {
     this.courses = await fetch.get(`/${path.PATH_COURSE}`);
     return this.courses;
   };
 
-  // getCourses = async () => {
-  //   this.courses = await fetch.get(`/${path.PATH_COURSE}`);
-  //   const wrapper = document.getElementsByClassName("course__list");
-  //     wrapper[0].innerHTML = "";
-  //     if (courses.length > 0 && Array.isArray(courses)) {
-  //       courses.forEach((course) => displayCourses(course));
-  //     }
-  //   return this.courses;
-  // };
-
+  getCourseById = async () => {
+    return this.course.find((item) => item.id === id);
+  };
 
   /**
    *
@@ -36,133 +31,67 @@ export default class Model {
    * @param {number} buyAmount
    * @param {string} bestSeller
    */
-  // course = {
-  //   image,
-  //   title,
-  //   author,
-  //   rating,
-  //   price,
-  //   buyAmount,
-  //   bestSeller
-  //  }
-  addNewCourse = async (
-    image,
-    title,
-    author,
-    rating,
-    price,
-    buyAmount,
-    bestSeller
-  ) => {
-    const course = await fetch.create(`/${path.PATH_COURSE}`, {
-      image,
-      title,
-      author,
-      rating,
-      price,
-      buyAmount,
-      bestSeller: "BestSeller",
-    });
-    console.log("model course", course);
+  addNewCourse = async (data) => {
+    const dataParam = {
+      id: uuidv4(),
+      image: data.image,
+      title: data.title,
+      author: data.author,
+      rating: data.rating,
+      price: data.price,
+      buyAmount: data.buyAmount,
+      bestSeller: data.bestSeller,
+    };
+    const course = await fetch.create(`/${path.PATH_COURSE}`, dataParam);
     this.courses.push(course);
-    return this.course;
-  };
-  
-  getCourseById = (id) => {
-      const index = this.courses.find((item) => item.id === id);
-      const course = this.courses[index];
+    return course;
   };
 
   /**
    * Use API url from fetch import and param id from controller in update
    * @param {string} id
-   * @param {string} updateImg
    */
-//   updateCourse = async (
-//     id,
-//     updateImg,
-//     updateTitle,
-//     updateAuthor,
-//     updateRating,
-//     updatePrice,
-//     updateBuyAmount,
-//     updateBestSeller
-//   ) => {
-//     const index = this.courses.findIndex((course) => course.id === id);
-//     const courseUpdate = {
-//       id,
-//       image: updateImg,
-//       title: updateTitle,
-//       author: updateAuthor,
-//       rating: updateRating,
-//       price: updatePrice,
-//       buyAmount: updateBuyAmount,
-//       bestSeller: updateBestSeller,
-//     };
-
-//     await fetch.update(`/${path.PATH_COURSE}/${id}`, courseUpdate);
-//     this.courses.splice(index, 1, courseUpdate);
-//     return this.courses;
-//   };
-//     };
-//   };
-
-updateCourse = async (
-    id,
-    image,
-    title,
-    author,
-    rating,
-    price,
-    buyAmount,
-    bestSeller
-  ) => {
-    const index = this.tasks.findIndex((item) => item.id === id);
-    const course = await fetch.update(`/${path.PATH_COURSE}/${id}`, {
-      id,
-      image,
-      title,
-      author,
-      rating,
-      price,
-      buyAmount,
-      bestSeller: false,
-    });
-
-    this.courses.splice(index, 1, course);
-    return this.courses;
+  updateCourse = async (data) => {
+    const dataParam = {
+      // id: uuidv4(),
+      image: data.image,
+      title: data.title,
+      author: data.author,
+      rating: data.rating,
+      price: data.price,
+      buyAmount: data.buyAmount,
+      bestSeller: data.bestSeller,
+    };
+    const course = await fetch.update(`/${path.PATH_COURSE}`, dataParam);
+    const index = this.courses.findIndex((course) => course.id === id);
+    this.courses.slice(index, 1, course);
+    return course;
   };
 
   deleteCourse = async (id) => {
-    const index = this.courses.findIndex(course => course.id === id)
-    const course = this.courses[index]
-    await fetch.remove(`/${path.PATH_COURSE}/${id}`, course)
-    this.courses.splice(index, 1)
-    return this.courses
+    const course = await fetch.remove(`/${path.PATH_COURSE}/${id}`);
+    const index = this.courses.findIndex((course) => course.id === id);
+    this.courses.splice(index, 1);
+    return course;
   };
-  
-   getCourseById = (id) => {
-        const index = this.courses.find((course) => course.id === id);
-        const course = this.courses[index];
-    };
 
   /**
    * Search all course that match
    * @returns {array} course
    */
-  searchCourse = async (title) => {
-    const course = await fetch.get(`/${path.PATH_COURSE}?q=${title}`);
-    this.courses = course;
-    return course;
-  };
+  // searchCourse = async (title) => {
+  //   const course = await fetch.get(`/${path.PATH_COURSE}?q=${title}`);
+  //   this.courses = course;
+  //   return course;
+  // };
 
   /**
    * Filter all course that match
    * @returns {array}
    */
-  filterCourse = async (title) => {
-    const course = await fetch.get(`/${path.PATH_COURSE}?title=${title}`);
-    this.courses = course;
-    return course;
-  };
+  // filterCourse = async (title) => {
+  //   const course = await fetch.get(`/${path.PATH_COURSE}?title=${title}`);
+  //   this.courses = course;
+  //   return course;
+  // };
 }
